7b411072520ee53bbd45654d4b2ba5633926647a
kiranbisarya <kiran.bisarya@outlook.com>
2019-02-09 09:33:52 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function submitAnswer() {
+  var radios = document.getElementsByName("choice");
+  var i = 0, len = radios.length;
+  var checked = false;
+  var userAnswer;
+  var msg = document.getElementById("msg-container").innerHTML;
+
+
+  for( ; i < len; i++ ) {
+     if(radios[i].checked) {
+       checked = true;
+       userAnswer = radios[i].value;
+     }
+  }
+  // if user click submit button without selecting any option, print "please select choice answer".
+  if(!checked) {
+    document.getElementById("msg-container").innerHTML = "Please select an answer"
+    return;
+  }
+  // Correct answer
+  else if(userAnswer === "Option1") {
+     document.getElementById("msg-container").innerHTML = "Answer is Correct!"
+  }
+  // Incorrect answer
+  else {
+     document.getElementById("msg-container").innerHTML = "Answer is Incorrect!"
+  }
+
+  // display the hidden msg.
+  document.getElementById("msg-container").style.display = "inline";
+
+}
